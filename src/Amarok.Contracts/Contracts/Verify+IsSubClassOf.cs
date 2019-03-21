1678486192bd712ffc1fffcdd36c1fc0e294411c
Olaf Kober <olaf.kober@outlook.com>
2019-03-21 22:47:52 +0100

    added Verify.IsSubclassOf(..)

---
+++ src/Amarok.Contracts/Contracts/Verify+IsSubClassOf.cs
@@ -0,0 +1,69 @@
+﻿/* Copyright(c) 2019, Olaf Kober
+ * Licensed under GNU Lesser General Public License v3.0 (LPGL-3.0).
+ * https://github.com/Amarok79/Amarok.Contracts
+ */
+
+#pragma warning disable S3218 // Inner class members should not shadow outer class "static" or type members
+
+using System;
+using System.Diagnostics;
+
+
+namespace Amarok.Contracts
+{
+	public partial class Verify
+	{
+		/// <summary>
+		/// Verifies that the given type is not null and derived from the specified base class.
+		/// </summary>
+		/// 
+		/// <param name="type">
+		/// The type to verify.</param>
+		/// <param name="baseType">
+		/// The expected base class.</param>
+		/// <param name="paramName">
+		/// The name of the method parameter that is verified.</param>
+		/// 
+		/// <exception cref="ArgumentNullException">
+		/// Null values are invalid.</exception>
+		/// <exception cref="ArgumentException">
+		/// Types not derived from a specific base class are invalid.</exception>
+		[DebuggerStepThrough]
+		public static void IsSubclassOf(Type type, Type baseType, String paramName)
+		{
+			if (type is null)
+				throw new ArgumentNullException(paramName, ExceptionResources.ArgumentNull);
+			if (!type.IsSubclassOf(baseType))
+				throw new ArgumentException(ExceptionResources.ArgumentIsSubclassOf, paramName);
+		}
+
+
+		public static partial class Debug
+		{
+			/// <summary>
+			/// Verifies that the given type is not null and derived from the specified base class.
+			/// </summary>
+			/// 
+			/// <param name="type">
+			/// The type to verify.</param>
+			/// <param name="baseType">
+			/// The expected base class.</param>
+			/// <param name="paramName">
+			/// The name of the method parameter that is verified.</param>
+			/// 
+			/// <exception cref="ArgumentNullException">
+			/// Null values are invalid.</exception>
+			/// <exception cref="ArgumentException">
+			/// Types not derived from a specific base class are invalid.</exception>
+			[Conditional("DEBUG")]
+			[DebuggerStepThrough]
+			public static void IsSubclassOf(Type type, Type baseType, String paramName)
+			{
+				if (type is null)
+					throw new ArgumentNullException(paramName, ExceptionResources.ArgumentNull);
+				if (!type.IsSubclassOf(baseType))
+					throw new ArgumentException(ExceptionResources.ArgumentIsSubclassOf, paramName);
+			}
+		}
+	}
+}
---
+++ src/Tests.Amarok.Contracts/Contracts/Test_Verify+IsSubClassOf.cs
@@ -0,0 +1,133 @@
+﻿/* Copyright(c) 2019, Olaf Kober
+ * Licensed under GNU Lesser General Public License v3.0 (LPGL-3.0).
+ * https://github.com/Amarok79/Amarok.Contracts
+ */
+
+#define DEBUG
+
+using System;
+using NFluent;
+using NUnit.Framework;
+
+
+namespace Amarok.Contracts
+{
+	public partial class Test_Verify
+	{
+		public class Animal
+		{
+		}
+		public sealed class Dog : Animal
+		{
+		}
+
+
+		[TestFixture]
+		public class IsSubclassOf
+		{
+			[Test]
+			public void DoesNotThrow_For_Instance()
+			{
+				Check.ThatCode(() => Verify.IsSubclassOf(typeof(Dog), typeof(Animal), "name"))
+					.DoesNotThrow();
+			}
+
+			[Test]
+			public void Throws_For_Null()
+			{
+				var exception = Check.ThatCode(() => Verify.IsSubclassOf(null, typeof(Animal), "name"))
+					.Throws<ArgumentNullException>()
+					.Value;
+
+				Check.That(exception.Message)
+					.StartsWith(ExceptionResources.ArgumentNull);
+				Check.That(exception.ParamName)
+					.IsEqualTo("name");
+				Check.That(exception.InnerException)
+					.IsNull();
+			}
+
+			[Test]
+			public void Throws_For_NullBaseType()
+			{
+				var exception = Check.ThatCode(() => Verify.IsSubclassOf(typeof(Dog), null, "name"))
+					.Throws<ArgumentNullException>()
+					.Value;
+
+				Check.That(exception.ParamName)
+					.IsEqualTo("type");
+				Check.That(exception.InnerException)
+					.IsNull();
+			}
+
+			[Test]
+			public void Throws()
+			{
+				var exception = Check.ThatCode(() => Verify.IsSubclassOf(typeof(Dog), typeof(Dog), "name"))
+					.Throws<ArgumentException>()
+					.Value;
+
+				Check.That(exception.Message)
+					.StartsWith(ExceptionResources.ArgumentIsSubclassOf);
+				Check.That(exception.ParamName)
+					.IsEqualTo("name");
+				Check.That(exception.InnerException)
+					.IsNull();
+			}
+		}
+
+		[TestFixture]
+		public class Debug_IsSubclassOf
+		{
+			[Test]
+			public void DoesNotThrow_For_Instance()
+			{
+				Check.ThatCode(() => Verify.Debug.IsSubclassOf(typeof(Dog), typeof(Animal), "name"))
+					.DoesNotThrow();
+			}
+
+			[Test]
+			public void Throws_For_Null()
+			{
+				var exception = Check.ThatCode(() => Verify.Debug.IsSubclassOf(null, typeof(Animal), "name"))
+					.Throws<ArgumentNullException>()
+					.Value;
+
+				Check.That(exception.Message)
+					.StartsWith(ExceptionResources.ArgumentNull);
+				Check.That(exception.ParamName)
+					.IsEqualTo("name");
+				Check.That(exception.InnerException)
+					.IsNull();
+			}
+
+			[Test]
+			public void Throws_For_NullBaseType()
+			{
+				var exception = Check.ThatCode(() => Verify.Debug.IsSubclassOf(typeof(Dog), null, "name"))
+					.Throws<ArgumentNullException>()
+					.Value;
+
+				Check.That(exception.ParamName)
+					.IsEqualTo("type");
+				Check.That(exception.InnerException)
+					.IsNull();
+			}
+
+			[Test]
+			public void Throws()
+			{
+				var exception = Check.ThatCode(() => Verify.Debug.IsSubclassOf(typeof(Dog), typeof(Dog), "name"))
+					.Throws<ArgumentException>()
+					.Value;
+
+				Check.That(exception.Message)
+					.StartsWith(ExceptionResources.ArgumentIsSubclassOf);
+				Check.That(exception.ParamName)
+					.IsEqualTo("name");
+				Check.That(exception.InnerException)
+					.IsNull();
+			}
+		}
+	}
+}
