a60754808c1f9bdbcf1b97c45c80750e0e0a8014
Olaf Kober <olaf.kober@outlook.com>
2019-04-21 22:18:32 +0200

    added Verify.IsValid(array, count)

---
+++ src/Amarok.Contracts/Contracts/Verify+IsValid.cs
@@ -0,0 +1,73 @@
+﻿/* Copyright(c) 2018, Olaf Kober
+ * Licensed under GNU Lesser General Public License v3.0 (LPGL-3.0).
+ * https://github.com/Amarok79/Amarok.Contracts
+ */
+
+#pragma warning disable S3218 // Inner class members should not shadow outer class "static" or type members
+
+using System;
+using System.Diagnostics;
+
+
+namespace Amarok.Contracts
+{
+	public partial class Verify
+	{
+		/// <summary>
+		/// Verifies that the given array and count represent a valid array segment.
+		/// </summary>
+		/// 
+		/// <param name="array">
+		/// The array.</param>
+		/// <param name="count">
+		/// The count.</param>
+		/// 
+		/// <exception cref="ArgumentNullException">
+		/// Null values are invalid.</exception>
+		/// <exception cref="ArgumentOutOfRangeException">
+		/// Negative values are invalid.</exception>
+		/// <exception cref="ArgumentExceedsUpperLimitException">
+		/// Values exceeding the inclusive upper limit are invalid.</exception>
+		[DebuggerStepThrough]
+		public static void IsValid<T>(T[] array, Int32 count)
+		{
+			if (array is null)
+				throw new ArgumentNullException(nameof(array), ExceptionResources.ArgumentNull);
+			if (count < 0)
+				throw new ArgumentOutOfRangeException(nameof(count), count, ExceptionResources.ArgumentIsPositive);
+			if (count > array.Length)
+				throw new ArgumentExceedsUpperLimitException(nameof(count), count, array.Length, ExceptionResources.ArgumentIsLessThan);
+		}
+
+
+		public static partial class Debug
+		{
+			/// <summary>
+			/// Verifies that the given array and count represent a valid array segment.
+			/// </summary>
+			/// 
+			/// <param name="array">
+			/// The array.</param>
+			/// <param name="count">
+			/// The count.</param>
+			/// 
+			/// <exception cref="ArgumentNullException">
+			/// Null values are invalid.</exception>
+			/// <exception cref="ArgumentOutOfRangeException">
+			/// Negative values are invalid.</exception>
+			/// <exception cref="ArgumentExceedsUpperLimitException">
+			/// Values exceeding the inclusive upper limit are invalid.</exception>
+			[Conditional("DEBUG")]
+			[DebuggerStepThrough]
+			public static void IsValid<T>(T[] array, Int32 count)
+			{
+				if (array is null)
+					throw new ArgumentNullException(nameof(array), ExceptionResources.ArgumentNull);
+				if (count < 0)
+					throw new ArgumentOutOfRangeException(nameof(count), count, ExceptionResources.ArgumentIsPositive);
+				if (count > array.Length)
+					throw new ArgumentExceedsUpperLimitException(nameof(count), count, array.Length, ExceptionResources.ArgumentIsLessThan);
+			}
+		}
+	}
+}
---
+++ src/Tests.Amarok.Contracts/Contracts/Test_Verify+IsValid.cs
@@ -0,0 +1,149 @@
+﻿/* Copyright(c) 2019, Olaf Kober
+ * Licensed under GNU Lesser General Public License v3.0 (LPGL-3.0).
+ * https://github.com/Amarok79/Amarok.Contracts
+ */
+
+#define DEBUG
+
+using System;
+using NFluent;
+using NUnit.Framework;
+
+
+namespace Amarok.Contracts
+{
+	public partial class Test_Verify
+	{
+		[TestFixture]
+		public class IsValid_Array_Count
+		{
+			[Test]
+			public void DoesNotThrow()
+			{
+				Check.ThatCode(() => Verify.IsValid(new Byte[2], 0))
+					.DoesNotThrow();
+				Check.ThatCode(() => Verify.IsValid(new Byte[2], 1))
+					.DoesNotThrow();
+				Check.ThatCode(() => Verify.IsValid(new Byte[2], 2))
+					.DoesNotThrow();
+			}
+
+			[Test]
+			public void ThrowsForNull()
+			{
+				var exception = Check.ThatCode(() => Verify.IsValid((Byte[])null, 0))
+					.Throws<ArgumentNullException>()
+					.Value;
+
+				Check.That(exception.Message)
+					.StartsWith(ExceptionResources.ArgumentNull);
+				Check.That(exception.ParamName)
+					.IsEqualTo("array");
+				Check.That(exception.InnerException)
+					.IsNull();
+			}
+
+			[Test]
+			public void ThrowsForNegativeCount()
+			{
+				var exception = Check.ThatCode(() => Verify.IsValid(new Byte[2], -1))
+					.Throws<ArgumentOutOfRangeException>()
+					.Value;
+
+				Check.That(exception.Message)
+					.StartsWith(ExceptionResources.ArgumentIsPositive);
+				Check.That(exception.ParamName)
+					.IsEqualTo("count");
+				Check.That(exception.InnerException)
+					.IsNull();
+				Check.That((Int32)exception.ActualValue)
+					.IsEqualTo(-1);
+			}
+
+			[Test]
+			public void ThrowsForCountTooBig()
+			{
+				var exception = Check.ThatCode(() => Verify.IsValid(new Byte[2], 3))
+					.Throws<ArgumentExceedsUpperLimitException>()
+					.Value;
+
+				Check.That(exception.Message)
+					.StartsWith(ExceptionResources.ArgumentIsLessThan);
+				Check.That(exception.ParamName)
+					.IsEqualTo("count");
+				Check.That(exception.InnerException)
+					.IsNull();
+				Check.That((Int32)exception.ActualValue)
+					.IsEqualTo(3);
+				Check.That((Int32)exception.UpperLimit)
+					.IsEqualTo(2);
+			}
+		}
+
+		[TestFixture]
+		public class Debug_IsValid_Array_Count
+		{
+			[Test]
+			public void DoesNotThrow()
+			{
+				Check.ThatCode(() => Verify.Debug.IsValid(new Byte[2], 0))
+					.DoesNotThrow();
+				Check.ThatCode(() => Verify.Debug.IsValid(new Byte[2], 1))
+					.DoesNotThrow();
+				Check.ThatCode(() => Verify.Debug.IsValid(new Byte[2], 2))
+					.DoesNotThrow();
+			}
+
+			[Test]
+			public void ThrowsForNull()
+			{
+				var exception = Check.ThatCode(() => Verify.Debug.IsValid((Byte[])null, 0))
+					.Throws<ArgumentNullException>()
+					.Value;
+
+				Check.That(exception.Message)
+					.StartsWith(ExceptionResources.ArgumentNull);
+				Check.That(exception.ParamName)
+					.IsEqualTo("array");
+				Check.That(exception.InnerException)
+					.IsNull();
+			}
+
+			[Test]
+			public void ThrowsForNegativeCount()
+			{
+				var exception = Check.ThatCode(() => Verify.Debug.IsValid(new Byte[2], -1))
+					.Throws<ArgumentOutOfRangeException>()
+					.Value;
+
+				Check.That(exception.Message)
+					.StartsWith(ExceptionResources.ArgumentIsPositive);
+				Check.That(exception.ParamName)
+					.IsEqualTo("count");
+				Check.That(exception.InnerException)
+					.IsNull();
+				Check.That((Int32)exception.ActualValue)
+					.IsEqualTo(-1);
+			}
+
+			[Test]
+			public void ThrowsForCountTooBig()
+			{
+				var exception = Check.ThatCode(() => Verify.Debug.IsValid(new Byte[2], 3))
+					.Throws<ArgumentExceedsUpperLimitException>()
+					.Value;
+
+				Check.That(exception.Message)
+					.StartsWith(ExceptionResources.ArgumentIsLessThan);
+				Check.That(exception.ParamName)
+					.IsEqualTo("count");
+				Check.That(exception.InnerException)
+					.IsNull();
+				Check.That((Int32)exception.ActualValue)
+					.IsEqualTo(3);
+				Check.That((Int32)exception.UpperLimit)
+					.IsEqualTo(2);
+			}
+		}
+	}
+}
