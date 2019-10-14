2f50f80634bea17676916087ccc962ef32abfe83
Olaf Kober <olaf.kober@outlook.com>
2019-10-14 23:13:39 +0200

    #6 Verify.NotEmptyOrWhiteSpace

---
+++ src/Amarok.Contracts/Contracts/Verify+NotEmptyOrWhiteSpace.cs
@@ -0,0 +1,65 @@
+﻿/* Copyright(c) 2019, Olaf Kober
+ * Licensed under GNU Lesser General Public License v3.0 (LPGL-3.0).
+ * https://github.com/Amarok79/Amarok.Contracts
+ */
+
+#pragma warning disable S3218 // Inner class members should not shadow outer class "static" or type members
+
+using System;
+using System.Diagnostics;
+
+
+namespace Amarok.Contracts
+{
+	public partial class Verify
+	{
+		/// <summary>
+		/// Verifies that the given value is neither null nor an empty or whitespace string.
+		/// </summary>
+		/// 
+		/// <param name="value">
+		/// The value to verify.</param>
+		/// <param name="paramName">
+		/// The name of the method parameter that is verified.</param>
+		/// 
+		/// <exception cref="ArgumentNullException">
+		/// Null values are invalid.</exception>
+		/// <exception cref="ArgumentException">
+		/// Empty strings are invalid.</exception>
+		[DebuggerStepThrough]
+		public static void NotEmptyOrWhiteSpace(String? value, String paramName)
+		{
+			if (value is null)
+				throw new ArgumentNullException(paramName, ExceptionResources.ArgumentNull);
+			if (String.IsNullOrWhiteSpace(value))
+				throw new ArgumentException(ExceptionResources.ArgumentEmptyString, paramName);
+		}
+
+
+		public static partial class Debug
+		{
+			/// <summary>
+			/// Verifies that the given value is neither null nor an empty or whitespace string.
+			/// </summary>
+			/// 
+			/// <param name="value">
+			/// The value to verify.</param>
+			/// <param name="paramName">
+			/// The name of the method parameter that is verified.</param>
+			/// 
+			/// <exception cref="ArgumentNullException">
+			/// Null values are invalid.</exception>
+			/// <exception cref="ArgumentException">
+			/// Empty strings are invalid.</exception>
+			[Conditional("DEBUG")]
+			[DebuggerStepThrough]
+			public static void NotEmptyOrWhiteSpace(String? value, String paramName)
+			{
+				if (value is null)
+					throw new ArgumentNullException(paramName, ExceptionResources.ArgumentNull);
+				if (String.IsNullOrWhiteSpace(value))
+					throw new ArgumentException(ExceptionResources.ArgumentEmptyString, paramName);
+			}
+		}
+	}
+}
---
+++ src/Tests.Amarok.Contracts/Contracts/Test_Verify+NotEmptyOrWhiteSpace.cs
@@ -0,0 +1,133 @@
+﻿/* Copyright(c) 2019, Olaf Kober
+ * Licensed under GNU Lesser General Public License v3.0 (LPGL-3.0).
+ * https://github.com/Amarok79/Amarok.Contracts
+ */
+
+#define DEBUG
+
+using System;
+using NFluent;
+using NUnit.Framework;
+
+
+namespace Amarok.Contracts
+{
+	public partial class Test_Verify
+	{
+		[TestFixture]
+		public class NotEmptyOrWhiteSpace_String
+		{
+			[Test]
+			public void DoesNotThrow()
+			{
+				Check.ThatCode(() => Verify.NotEmptyOrWhiteSpace("foo", "name"))
+					.DoesNotThrow();
+				Check.ThatCode(() => Verify.NotEmptyOrWhiteSpace(" x ", "name"))
+					.DoesNotThrow();
+			}
+
+			[Test]
+			public void Throws_For_Null()
+			{
+				var exception = Check.ThatCode(() => Verify.NotEmptyOrWhiteSpace((String)null, "name"))
+					.Throws<ArgumentNullException>()
+					.Value;
+
+				Check.That(exception.Message)
+					.StartsWith(ExceptionResources.ArgumentNull);
+				Check.That(exception.ParamName)
+					.IsEqualTo("name");
+				Check.That(exception.InnerException)
+					.IsNull();
+			}
+
+			[Test]
+			public void Throws_For_EmptyString()
+			{
+				var exception = Check.ThatCode(() => Verify.NotEmptyOrWhiteSpace(String.Empty, "name"))
+					.Throws<ArgumentException>()
+					.Value;
+
+				Check.That(exception.Message)
+					.StartsWith(ExceptionResources.ArgumentEmptyString);
+				Check.That(exception.ParamName)
+					.IsEqualTo("name");
+				Check.That(exception.InnerException)
+					.IsNull();
+			}
+
+			[Test]
+			public void Throws_For_WhitespaceString()
+			{
+				var exception = Check.ThatCode(() => Verify.NotEmptyOrWhiteSpace(" ", "name"))
+					.Throws<ArgumentException>()
+					.Value;
+
+				Check.That(exception.Message)
+					.StartsWith(ExceptionResources.ArgumentEmptyString);
+				Check.That(exception.ParamName)
+					.IsEqualTo("name");
+				Check.That(exception.InnerException)
+					.IsNull();
+			}
+		}
+
+		[TestFixture]
+		public class Debug_NotEmptyOrWhiteSpace_String
+		{
+			[Test]
+			public void DoesNotThrow()
+			{
+				Check.ThatCode(() => Verify.Debug.NotEmptyOrWhiteSpace("foo", "name"))
+					.DoesNotThrow();
+				Check.ThatCode(() => Verify.Debug.NotEmptyOrWhiteSpace(" x ", "name"))
+					.DoesNotThrow();
+			}
+
+			[Test]
+			public void Throws_For_Null()
+			{
+				var exception = Check.ThatCode(() => Verify.Debug.NotEmptyOrWhiteSpace(null, "name"))
+					.Throws<ArgumentNullException>()
+					.Value;
+
+				Check.That(exception.Message)
+					.StartsWith(ExceptionResources.ArgumentNull);
+				Check.That(exception.ParamName)
+					.IsEqualTo("name");
+				Check.That(exception.InnerException)
+					.IsNull();
+			}
+
+			[Test]
+			public void Throws_For_EmptyString()
+			{
+				var exception = Check.ThatCode(() => Verify.Debug.NotEmptyOrWhiteSpace(String.Empty, "name"))
+					.Throws<ArgumentException>()
+					.Value;
+
+				Check.That(exception.Message)
+					.StartsWith(ExceptionResources.ArgumentEmptyString);
+				Check.That(exception.ParamName)
+					.IsEqualTo("name");
+				Check.That(exception.InnerException)
+					.IsNull();
+			}
+
+			[Test]
+			public void Throws_For_WhitespaceString()
+			{
+				var exception = Check.ThatCode(() => Verify.Debug.NotEmptyOrWhiteSpace(" ", "name"))
+					.Throws<ArgumentException>()
+					.Value;
+
+				Check.That(exception.Message)
+					.StartsWith(ExceptionResources.ArgumentEmptyString);
+				Check.That(exception.ParamName)
+					.IsEqualTo("name");
+				Check.That(exception.InnerException)
+					.IsNull();
+			}
+		}
+	}
+}
