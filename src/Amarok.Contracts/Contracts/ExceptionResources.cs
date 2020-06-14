fa1616536e2087add07b13b07ceeeac1256fea16
Olaf Kober <olaf.kober@outlook.com>
2020-06-14 22:37:18 +0200

    converted resx to constant strings

---
+++ src/Amarok.Contracts/Contracts/ExceptionResources.cs
@@ -0,0 +1,110 @@
+﻿/* MIT License
+ * 
+ * Copyright (c) 2020, Olaf Kober
+ * https://github.com/Amarok79/Amarok.Contracts
+ * 
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ * 
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+*/
+
+using System;
+
+
+namespace Amarok.Contracts
+{
+	/// <summary>
+	/// This type serves the infrastructure and is not intended to be used directly.
+	/// </summary>
+	internal static class ExceptionResources
+	{
+		/// <summary>
+		///   Looks up a localized string similar to Empty collections are invalid..
+		/// </summary>
+		internal static String ArgumentEmptyCollection => "Empty collections are invalid.";
+
+		/// <summary>
+		///   Looks up a localized string similar to Empty strings are invalid..
+		/// </summary>
+		internal static String ArgumentEmptyString => "Empty strings are invalid.";
+
+		/// <summary>
+		///   Looks up a localized string similar to Types not assignable to a specific type are invalid..
+		/// </summary>
+		internal static String ArgumentIsAssignableTo => "Types not assignable to a specific type are invalid.";
+
+		/// <summary>
+		///   Looks up a localized string similar to Values exceeding the inclusive lower limit are invalid..
+		/// </summary>
+		internal static String ArgumentIsGreaterThan => "Values exceeding the inclusive lower limit are invalid.";
+
+		/// <summary>
+		///   Looks up a localized string similar to Types representing interface or abstract base classes are invalid..
+		/// </summary>
+		internal static String ArgumentIsInstantiable => "Types representing interface or abstract base classes are invalid.";
+
+		/// <summary>
+		///   Looks up a localized string similar to Types representing concrete classes or value types are invalid..
+		/// </summary>
+		internal static String ArgumentIsInterface => "Types representing concrete classes or value types are invalid.";
+
+		/// <summary>
+		///   Looks up a localized string similar to Values exceeding the inclusive upper limit are invalid..
+		/// </summary>
+		internal static String ArgumentIsLessThan => "Values exceeding the inclusive upper limit are invalid.";
+
+		/// <summary>
+		///   Looks up a localized string similar to Negative values are invalid..
+		/// </summary>
+		internal static String ArgumentIsPositive => "Negative values are invalid.";
+
+		/// <summary>
+		///   Looks up a localized string similar to Values exceeding the exclusive lower limit are invalid..
+		/// </summary>
+		internal static String ArgumentIsStrictlyGreaterThan => "Values exceeding the exclusive lower limit are invalid.";
+
+		/// <summary>
+		///   Looks up a localized string similar to Values exceeding the exclusive upper limit are invalid..
+		/// </summary>
+		internal static String ArgumentIsStrictlyLessThan => "Values exceeding the exclusive upper limit are invalid.";
+
+		/// <summary>
+		///   Looks up a localized string similar to Zero or negative values are invalid..
+		/// </summary>
+		internal static String ArgumentIsStrictlyPositive => "Zero or negative values are invalid.";
+
+		/// <summary>
+		///   Looks up a localized string similar to Types not derived from a specific base class are invalid..
+		/// </summary>
+		internal static String ArgumentIsSubclassOf => "Types not derived from a specific base class are invalid.";
+
+		/// <summary>
+		///   Looks up a localized string similar to Null values are invalid..
+		/// </summary>
+		internal static String ArgumentNull => "Null values are invalid.";
+
+		/// <summary>
+		///   Looks up a localized string similar to Lower limit: .
+		/// </summary>
+		internal static String LowerLimit => "Lower limit: ";
+
+		/// <summary>
+		///   Looks up a localized string similar to Upper limit: .
+		/// </summary>
+		internal static String UpperLimit => "Upper limit: ";
+	}
+}
