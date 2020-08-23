7e702b6def67bea9718001533e660cdc94d5ac86
Olaf Kober <olaf.kober@outlook.com>
2020-08-23 19:30:16 +0200

    NotNull benchmarks

--- src/Benchmarks.Amarok.Contracts/Contracts/NotNullBechmarks.cs
+++ src/Benchmarks.Amarok.Contracts/Contracts/NotNullBechmarks.cs
@@ -37,7 +37,7 @@ namespace Amarok.Contracts
 		[Benchmark(Baseline = true)]
 		public void Baseline()
 		{
-			for (Int32 i = 0; i < 10000; i++)
+			for (Int32 i = 0; i < 1000; i++)
 				_BaselineCore("abc");
 		}
 
@@ -50,7 +50,7 @@ namespace Amarok.Contracts
 		[Benchmark]
 		public void Throw()
 		{
-			for (Int32 i = 0; i < 10000; i++)
+			for (Int32 i = 0; i < 1000; i++)
 				_ThrowCore("abc");
 		}
 
@@ -65,7 +65,7 @@ namespace Amarok.Contracts
 		[Benchmark]
 		public void Verify()
 		{
-			for (Int32 i = 0; i < 10000; i++)
+			for (Int32 i = 0; i < 1000; i++)
 				_VerifyCore("abc");
 		}
 
@@ -76,3 +76,22 @@ namespace Amarok.Contracts
 		}
 	}
 }
+
+/*
+BenchmarkDotNet=v0.12.1, OS=Windows 10.0.19041.450 (2004/?/20H1)
+Intel Core i7-8750H CPU 2.20GHz (Coffee Lake), 1 CPU, 12 logical and 6 physical cores
+  [Host]        : .NET Framework 4.8 (4.8.4200.0), X64 RyuJIT
+  .NET 4.8      : .NET Framework 4.8 (4.8.4200.0), X64 RyuJIT
+  .NET Core 3.1 : .NET Core 3.1.7 (CoreCLR 4.700.20.36602, CoreFX 4.700.20.37001), X64 RyuJIT
+
+
+|   Method |           Job |       Runtime |     Mean |     Error |    StdDev | Ratio |
+|--------- |-------------- |-------------- |---------:|----------:|----------:|------:|
+| Baseline |      .NET 4.8 |      .NET 4.8 | 1.277 us | 0.0037 us | 0.0034 us |  1.00 |
+|    Throw |      .NET 4.8 |      .NET 4.8 | 1.531 us | 0.0033 us | 0.0029 us |  1.20 |
+|   Verify |      .NET 4.8 |      .NET 4.8 | 1.802 us | 0.0017 us | 0.0015 us |  1.41 |
+|          |               |               |          |           |           |       |
+| Baseline | .NET Core 3.1 | .NET Core 3.1 | 1.278 us | 0.0038 us | 0.0035 us |  1.00 |
+|    Throw | .NET Core 3.1 | .NET Core 3.1 | 1.277 us | 0.0034 us | 0.0032 us |  1.00 |
+|   Verify | .NET Core 3.1 | .NET Core 3.1 | 1.532 us | 0.0041 us | 0.0038 us |  1.20 |
+*/
