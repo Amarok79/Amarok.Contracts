9944181fedf586ab281e29df4f9ea6e46ca4466f
Olaf Kober <olaf.kober@outlook.com>
2022-01-28 19:28:08 +0100

    reworked projects, packing, etc.

--- _pack.cmd
+++
@@ -1,9 +0,0 @@
-@ECHO OFF
-
-nuget.exe pack Amarok.Contracts.nuspec -OutputDirectory bin\lib
-
-ECHO.
-ECHO ====================================================================================================
-ECHO	Batch execution finished. Press any key to quit!
-ECHO ====================================================================================================
-PAUSE
